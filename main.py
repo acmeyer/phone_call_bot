31f7c5255069236b55d7c15db3defe464c639f68
Alex Meyer <acomeyer@gmail.com>
2024-05-25 12:00:47 -0500

    moderize it a bit

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import os
 import datetime
-import openai
+from openai import OpenAI
+client = OpenAI()
 import argparse
 from dataclasses import asdict
 from models import Message
 import speech_recognition as sr
 import logging
+from pathlib import Path
 logging.basicConfig(level=logging.INFO)
 import dotenv
 dotenv.load_dotenv('.env')
-import requests
 
-CHAT_MODEL = "gpt-3.5-turbo"
+CHAT_MODEL = "gpt-4o"
 MODEL_TEMPERATURE = 0.5
 AUDIO_MODEL = "whisper-1"
-ELEVEN_LABS_API_KEY = os.getenv('ELEVEN_LABS_API_KEY')
-ELEVEN_LABS_VOICE_ID = os.getenv('ELEVEN_LABS_VOICE_ID')
 
-def ask_gpt3_chat(prompt: str, messages: list[Message]):
-    """Returns ChatGPT-3's response to the given prompt."""
+def ask_gpt_chat(prompt: str, messages: list[Message]):
+    """Returns ChatGPT's response to the given prompt."""
     system_message = [{"role": "system", "content": prompt}]
     message_dicts = [asdict(message) for message in messages]
     conversation_messages = system_message + message_dicts
-    response = openai.ChatCompletion.create(
-        model=CHAT_MODEL,
-        messages=conversation_messages,
-        temperature=MODEL_TEMPERATURE
-    )
-    return response.choices[0]['message']['content'].strip()
+    response = client.chat.completions.create(model=CHAT_MODEL,
+    messages=conversation_messages,
+    temperature=MODEL_TEMPERATURE)
+    return response.choices[0].message.content
 
 def setup_prompt(prompt_file: str = 'prompts/vet_prompt.md') -> str:
-    """Creates a prompt for gpt-3 for generating a response."""
+    """Creates a prompt for gpt for generating a response."""
     with open(prompt_file) as f:
         prompt = f.read()
 
@@ -38,10 +35,11 @@ def setup_prompt(prompt_file: str = 'prompts/vet_prompt.md') -> str:
 
 def get_transcription(file_path: str):
     audio_file= open(file_path, "rb")
-    return openai.Audio.transcribe(
+    transcription = client.audio.transcriptions.create(
         model=AUDIO_MODEL, 
         file=audio_file
     )
+    return transcription.text
 
 def record():
     # load the speech recognizer with CLI settings
@@ -61,32 +59,19 @@ def record():
         f.write(audio.get_wav_data())
     transcript = get_transcription(f"./recordings/{timestamp}.wav")
     with open(f"./transcripts/{timestamp}.txt", "w") as f:
-        f.write(transcript['text'])
-    return transcript['text']
+        f.write(transcript)
+    return transcript
 
 def text_to_speech(text: str):
-    url = f'https://api.elevenlabs.io/v1/text-to-speech/{ELEVEN_LABS_VOICE_ID}'
-    headers = {
-        'accept': 'audio/mpeg',
-        'xi-api-key': ELEVEN_LABS_API_KEY,
-        'Content-Type': 'application/json'
-    }
-    data = {
-        'text': text,
-        'voice_settings': {
-            'stability': 0.5,
-            'similarity_boost': 0.25
-        }
-    }
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
-        timestamp = datetime.datetime.now().timestamp()
-        with open(f"outputs/{timestamp}.mp3", "wb") as out:
-            # Write the response to the output file.
-            out.write(response.content)
-        return f"outputs/{timestamp}.mp3"
-    else:
-        return None
+    timestamp = datetime.datetime.now().timestamp()
+    speech_file_path = Path(__file__).parent / f"outputs/{timestamp}.mp3"
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text
+    )
+    response.write_to_file(speech_file_path)
+    return speech_file_path
 
 def clean_up():
     logging.info('Exiting...')
@@ -108,7 +93,7 @@ if __name__ == "__main__":
     parser.add_argument("-pf", "--prompt_file", help="Specify the prompt file to use.", type=str)
     args = parser.parse_args()
     prompt_file = args.prompt_file
-    
+
     prompt = setup_prompt(prompt_file)
     conversation_messages = []
     while True:
@@ -116,7 +101,7 @@ if __name__ == "__main__":
             user_input = record()
             logging.info(f'Receiver: {user_input}')
             conversation_messages.append(Message(role="user", content=user_input))
-            answer = ask_gpt3_chat(prompt, conversation_messages)
+            answer = ask_gpt_chat(prompt, conversation_messages)
             logging.info(f'Caller: {answer}')
             logging.info('Playing audio...')
             audio_file = text_to_speech(answer)
